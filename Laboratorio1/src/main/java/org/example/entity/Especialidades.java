5133a3372a25620339071ce47e72b79935c26d2e
Alison Argueta <105072904+AlisonArgue@users.noreply.github.com>
2025-04-04 01:37:05 -0600

    :sparkles: Agregando lista de especialidades

---
+++ Laboratorio1/src/main/java/org/example/entity/Especialidades.java
@@ -0,0 +1,18 @@
+package org.example.entity;
+
+import java.util.*;
+
+public class Especialidades {
+    public static final Map<Integer, String> especialidades = Map.of(
+            1, "Medicina General",
+            2, "Medicina Interna",
+            3, "Pediatría",
+            4, "Cardiología",
+            5, "Dermatología",
+            6, "Neurología",
+            7, "Ginecología",
+            8, "Psiquiatría",
+            9, "Odontología",
+            10, "Oftalmología"
+    );
+}
